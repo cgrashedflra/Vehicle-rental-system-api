c19917b89e949cf8f00c8e94c0ea298e85e31238
Ashfaq <bdrasedas@gmail.com>
2025-12-24 12:57:34 +0600

    chore: remove dist from repository and ignore it

--- dist/utils/validateEmail.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateEmail = void 0;
-const validateEmail = (email) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-};
-exports.validateEmail = validateEmail;
